be305a147c447609d1fc410be603ef67c1eb3ab1
hezhongfeng <287645587@qq.com>
2024-02-08 15:45:44 +0800

    ✨ feat(core): 添加基础逻辑和 callback

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,31 @@
-const DetectBrowserNavigationInVueRouter = () => {
-  console.log('Hello from DetectBrowserNavigationInVueRouter');
+import { Router } from 'vue-router';
+
+interface DetectBrowserNavigationInVueRouterOption {
+  router: Router;
+  backNavigation: Function;
+  forwardNavigation: Function;
+}
+
+const DetectBrowserNavigationInVueRouter = {
+  install(_app: any, { router, backNavigation, forwardNavigation }: DetectBrowserNavigationInVueRouterOption) {
+    // 配置此应用
+    let navigationInfo: { direction: string } | null = null;
+    router.options.history.listen((_to, _from, info) => {
+      navigationInfo = info;
+    });
+
+    router.beforeEach(() => {
+      if (navigationInfo) {
+        if (navigationInfo.direction === 'back') {
+          backNavigation();
+        } else if (navigationInfo.direction === 'forward') {
+          forwardNavigation();
+        }
+        // need to reset it to null
+        navigationInfo = null;
+      }
+    });
+  }
 };
 
 export default DetectBrowserNavigationInVueRouter;
